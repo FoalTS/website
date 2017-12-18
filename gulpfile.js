eccc1fafd3a5bd4430e555a3d682255f7b07f68f
Loïc Poullain <loic.poullain@centraliens.net>
2017-12-18 11:01:10 +0100

    Add documentation (draft)

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,72 @@
 const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const ejs = require('gulp-ejs');
+const ext_replace = require('gulp-ext-replace');
+const header = require('gulp-header');
+const footer = require('gulp-footer');
+
+gulp.task('markdown-to-html', () => {
+  gulp.src('docs/**/*.md')
+    .pipe(header('${content}', { content: '<%- include(\'../partials/doc-top.html\') %>' }))
+    .pipe(footer('${content}', { content: '<%- include(\'../partials/doc-bottom.html\') %>' }))
+    .pipe(ext_replace('.html'))
+    .pipe(gulp.dest('templates/pages'))
+})
 
 gulp.task('copy', () => {
   gulp.src([
       'node_modules/bootstrap/dist/**/*',
       '!**/*.map'
     ])
-    .pipe(gulp.dest('assets/3p/bootstrap'));
+    .pipe(gulp.dest('dist/assets/3p/bootstrap'));
 
   gulp.src([
       'node_modules/jquery/dist/**/*',
       '!**/*.map'
     ])
-    .pipe(gulp.dest('assets/3p/jquery'));
+    .pipe(gulp.dest('dist/assets/3p/jquery'));
   
   gulp.src([
       'node_modules/typed.js/lib/**/*',
       '!**/*.map'
     ])
-    .pipe(gulp.dest('assets/3p/typed.js'));
+    .pipe(gulp.dest('dist/assets/3p/typed.js'));
 
   gulp.src('img/*')
-    .pipe(gulp.dest('assets/img'));
+    .pipe(gulp.dest('dist/assets/img'));
 });
-
-gulp.task('default', ['copy', 'sass']);
 
 gulp.task('sass', () => {
   gulp.src('scss/main.scss')
     .pipe(sass())
-    .pipe(gulp.dest('assets/css'))
+    .pipe(gulp.dest('dist/assets/css'))
     .pipe(browserSync.reload({
       stream: true
     }))
-})
+});
+
+gulp.task('html', ['markdown-to-html'], () => {
+  gulp.src('templates/pages/**/*.html')
+    .pipe(ejs({}))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('build', ['copy', 'sass', 'html']);
 
 gulp.task('browserSync', () => {
   browserSync.init({
     server: {
-      baseDir: ''
+      baseDir: './dist'
     },
   });
 });
 
-gulp.task('dev', ['browserSync', 'sass'], () => {
+gulp.task('dev', ['browserSync', 'build'], () => {
   gulp.watch('scss/*.scss', ['sass']);
-  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('docs/**/*.md', ['markdown-to-html']);
+  gulp.watch('templates/**/*.html', ['html']);
 });
