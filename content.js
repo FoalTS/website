1c3ee44d6188356efeac8ec3461cbbb76d18a5f6
Loïc Poullain <loic.poullain@centraliens.net>
2018-06-21 22:59:40 +0200

    Make the website catchier.

--- content.js
+++ content.js
@@ -6,32 +6,32 @@ module.exports = {
     {
       name: 'Dev tools',
       icon: 'wrench',
-      description: 'sdq'
+      description: 'Nisi mihi Phaedrum, inquam, tu mentitum aut Zenonem putas, quorum utrumque audivi, cum mihi nihil sane praeter sedulitatem probarent, omnes mihi Epicuri sententiae satis notae sunt. atque eos, quos nominavi, cum Attico nostro frequenter audivi, cum miraretur ille quidem utrumque, Phaedrum autem'
     },
     {
       name: 'TypeScript',
       icon: '',
-      description: ''
+      description: 'Post hoc impie perpetratum quod in aliis quoque iam timebatur, tamquam licentia crudelitati indulta per suspicionum nebulas aestimati quidam noxii damnabantur. quorum pars necati, alii puniti bonorum multatione actique laribus suis extorres nullo sibi relicto praeter querelas et lacrimas, stipe'
     },
     {
       name: 'Basic',
       icon: 'cubes',
-      description: ''
+      description: 'Ergo ego senator inimicus, si ita vultis, homini, amicus esse, sicut semper fui, rei publicae debeo. Quid? si ipsas inimicitias, depono rei publicae causa, quis me tandem iure reprehendet, praesertim cum ego omnium meorum consiliorum atque factorum exempla semper ex summorum'
     },
     {
       name: 'Plugins',
       icon: 'plug',
-      description: 'dqs'
+      description: 'Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.'
     },
     {
       name: 'archi',
       icon: 'building',
-      description: 'guideline'
+      description: 'guideline Ergo ego senator inimicus, si ita vultis, homini, amicus esse, sicut semper fui, rei publicae debeo. Quid? si ipsas inimicitias, depono rei publicae causa, quis me tandem iure reprehendet, praesertim cum ego omnium meorum consiliorum atque factorum exempla semper ex summorum'
     },
     {
       name: 'Reduce gap',
       icon: 'ship',
-      description: ''
+      description: 'Oportunum est, ut arbitror, explanare nunc causam, quae ad exitium praecipitem Aginatium inpulit iam inde a priscis maioribus nobilem, ut locuta est pertinacior fama. nec enim super hoc ulla documentorum rata est fides.'
     },
   ]
 }
