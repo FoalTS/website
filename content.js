a006f941185865d396096f41d13f39682c9aa4f5
Loïc Poullain <loic.poullain@centraliens.net>
2018-06-21 17:22:34 +0200

    Use gulp-ejs and font-awesome.

---
+++ content.js
@@ -0,0 +1,37 @@
+module.exports = {
+  title: 'FoalTS',
+  subtitle: 'Start fast, code less, do more.',
+  description: 'FoalTS is a high-level Node.JS framework to quickly build web apps in TypeScript. By providing a robust architecture with many packages and tools, FoalTS is designed to let you fastly bootstrap and develop ambitious backend projects.',
+  features: [
+    {
+      name: 'Dev tools',
+      icon: 'wrench',
+      description: 'sdq'
+    },
+    {
+      name: 'TypeScript',
+      icon: '',
+      description: ''
+    },
+    {
+      name: 'Basic',
+      icon: 'cubes',
+      description: ''
+    },
+    {
+      name: 'Plugins',
+      icon: 'plug',
+      description: 'dqs'
+    },
+    {
+      name: 'archi',
+      icon: 'building',
+      description: 'guideline'
+    },
+    {
+      name: 'Reduce gap',
+      icon: 'ship',
+      description: ''
+    },
+  ]
+}
