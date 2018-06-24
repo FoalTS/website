2bb3f12115fd195a757ed64b4956d38dc585e3c2
Loïc Poullain <loic.poullain@centraliens.net>
2018-06-24 16:45:07 +0200

    Update content.js

--- content.js
+++ content.js
@@ -4,34 +4,34 @@ module.exports = {
   description: 'FoalTS is a high-level Node.JS framework to quickly build web apps in TypeScript. By providing a robust architecture with many packages and tools, FoalTS is designed to let you fastly bootstrap and develop ambitious backend projects.',
   features: [
     {
+      name: 'Production-ready',
+      icon: 'ship',
+      description: 'Oportunum est, ut arbitror, explanare nunc causam, quae ad exitium praecipitem Aginatium inpulit iam inde a priscis maioribus nobilem, ut locuta est pertinacior fama. nec enim super hoc ulla documentorum rata est fides.'
+    },
+    {
+      name: 'TypeScript',
+      icon: 'code',
+      description: 'Post hoc impie perpetratum quod in aliis quoque iam timebatur, tamquam licentia crudelitati indulta per suspicionum nebulas aestimati quidam noxii damnabantur. quorum pars necati, alii puniti bonorum multatione actique laribus suis extorres nullo sibi relicto praeter querelas et lacrimas, stipe'
+    },
+    {
       name: 'Dev tools',
       icon: 'wrench',
       description: 'Nisi mihi Phaedrum, inquam, tu mentitum aut Zenonem putas, quorum utrumque audivi, cum mihi nihil sane praeter sedulitatem probarent, omnes mihi Epicuri sententiae satis notae sunt. atque eos, quos nominavi, cum Attico nostro frequenter audivi, cum miraretur ille quidem utrumque, Phaedrum autem'
     },
     {
-      name: 'TypeScript',
-      icon: '',
-      description: 'Post hoc impie perpetratum quod in aliis quoque iam timebatur, tamquam licentia crudelitati indulta per suspicionum nebulas aestimati quidam noxii damnabantur. quorum pars necati, alii puniti bonorum multatione actique laribus suis extorres nullo sibi relicto praeter querelas et lacrimas, stipe'
-    },
-    {
-      name: 'Basic',
+      name: 'Basic components',
       icon: 'cubes',
       description: 'Ergo ego senator inimicus, si ita vultis, homini, amicus esse, sicut semper fui, rei publicae debeo. Quid? si ipsas inimicitias, depono rei publicae causa, quis me tandem iure reprehendet, praesertim cum ego omnium meorum consiliorum atque factorum exempla semper ex summorum'
     },
     {
-      name: 'Plugins',
+      name: 'Plugins (soon)',
       icon: 'plug',
       description: 'Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.'
     },
     {
-      name: 'archi',
-      icon: 'building',
+      name: 'Security',
+      icon: 'shield',
       description: 'guideline Ergo ego senator inimicus, si ita vultis, homini, amicus esse, sicut semper fui, rei publicae debeo. Quid? si ipsas inimicitias, depono rei publicae causa, quis me tandem iure reprehendet, praesertim cum ego omnium meorum consiliorum atque factorum exempla semper ex summorum'
-    },
-    {
-      name: 'Reduce gap',
-      icon: 'ship',
-      description: 'Oportunum est, ut arbitror, explanare nunc causam, quae ad exitium praecipitem Aginatium inpulit iam inde a priscis maioribus nobilem, ut locuta est pertinacior fama. nec enim super hoc ulla documentorum rata est fides.'
     },
   ]
 }
