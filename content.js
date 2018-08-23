0deb08a7954609c5b34e0c4066ba1b697633998b
Loïc Poullain <loic.poullain@centraliens.net>
2018-08-23 12:38:39 +0200

    Improve description

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 module.exports = {
   title: 'FoalTS',
   subtitle: 'Create enterprise-grade Node.JS applications.',
-  description: 'FoalTS is a high-level Node.JS framework to quickly build web apps in TypeScript. Thanks to its architecture, packages and tools you can now bootstrap and develop entreprise-grade apps.',
   features: [
     {
       name: 'Production-ready',
